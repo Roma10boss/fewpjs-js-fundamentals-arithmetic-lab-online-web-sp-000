eb3c1e5e0c72a1b65d50e20429eb51aa75e7c674
Romario Gustave <romagustave10@gmail.com>
2021-01-28 16:59:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Write your code here
 const num1 = 31;
 const num2=2;
-multiply = num1*num2;
-const random = Math.random();
-const mod = 14/5;
-const max = Math.max(1, 2, 3, 4, 5, 20);
+const multiply = num1 * num2;
+
+const random = Math.random() + 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 8, 3, 20);
