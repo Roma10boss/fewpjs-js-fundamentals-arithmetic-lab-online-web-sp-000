4bc05ec2985e429c5db7a1ddd03e93da364cc9a5
Romario Gustave <romagustave10@gmail.com>
2021-01-28 16:59:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const num1 = 31;
 const num2=2;
 multiply = num1*num2;
-random = Math.random();
-mod = 14/5;
-max = Math.max(1, 2, 3, 4, 5, 20);
+const random = Math.random();
+const mod = 14/5;
+const max = Math.max(1, 2, 3, 4, 5, 20);
